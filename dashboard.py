2e757479903865f0a880c73a3bcf52f53a8ccb3b
thummaganti1997-sys <thummaganti1997@gmail.com>
2026-08-23 00:27:43 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,5 @@
-config = {
-    "developer_token": "XXXX",
-    "client_id": "XXXX",
-    "client_secret": "XXXX",
-    "refresh_token": "XXXX",
-    "login_customer_id": "XXXX",
-    "use_proto_plus": True
-}
+import streamlit as st
+
+st.title("Google Ads Dashboard")
+
+st.write("Dashboard is working successfully!")
