79c5578d8265d18280a594964e75a311beddeb07
thummaganti1997-sys <thummaganti1997@gmail.com>
2026-08-22 22:19:59 +0530

    Create dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,8 @@
+config = {
+    "developer_token": "XXXX",
+    "client_id": "XXXX",
+    "client_secret": "XXXX",
+    "refresh_token": "XXXX",
+    "login_customer_id": "XXXX",
+    "use_proto_plus": True
+}
